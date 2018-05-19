0cb9156e32a3b55a0ecfa98bc5e0b84fe32d6e91
joao garcia <joaogarcialn@hotmail.com>
2018-05-19 15:12:53 -0300

    Alterados testes e diretório da github_api close #2

---
+++ libpythonpro/github_api.py
@@ -0,0 +1,16 @@
+import requests
+
+
+def buscar_avatar(usuario):
+    '''
+    Busca o avatar de um usuário no Github
+
+    :param usuario: str Com o nome de um usuário no gitbub
+    :return: str com o link para o avatar
+    '''
+    url = f'https://api.github.com/users/{usuario}'
+    resp = requests.get(url)
+    return resp.json()['avatar_url']
+
+if __name__ == '__main__':
+    print(buscar_avatar('joaogarciadelima'))
