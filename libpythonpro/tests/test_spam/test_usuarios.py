2dbedb39ab6a95bb705de81eb0a8ffc974e221e3
joao garcia <joaogarcialn@hotmail.com>
2018-05-22 23:42:27 -0300

    Adicionado arquivo conftest

--- libpythonpro/tests/test_spam/test_usuarios.py
+++ libpythonpro/tests/test_spam/test_usuarios.py
@@ -1,22 +1,5 @@
-import pytest
-from libpythonpro.spam.db import Conexao
 from libpythonpro.spam.models import Usuario
 
-
-@pytest.fixture(scope='session')
-def conexao():
-    #Setup
-    conexao_obj = Conexao()
-    yield conexao_obj
-    # Tear Down
-    conexao_obj.fechar()
-
-@pytest.fixture
-def sessao(conexao):
-    sessao_obj = conexao.gerar_sessao()
-    yield sessao_obj
-    sessao_obj.roll_back()
-    sessao_obj.fechar()
 
 def test_salvar_usuario(sessao):
     usuario = Usuario (nome='joao')
