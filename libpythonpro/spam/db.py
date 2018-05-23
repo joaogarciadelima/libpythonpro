1f46ce0c253a17936707ce6cc43b05ec580f9a27
joao garcia <joaogarcialn@hotmail.com>
2018-05-23 00:18:21 -0300

    Ajustado o código no flake8

--- libpythonpro/spam/db.py
+++ libpythonpro/spam/db.py
@@ -4,6 +4,7 @@ from time import sleep
 class Sessao():
     contador = 0
     usuarios = []
+
     def salvar(self, usuario):
         Sessao.contador += 1
         usuario.id = Sessao.contador
--- libpythonpro/tests/test_spam/test_usuarios.py
+++ libpythonpro/tests/test_spam/test_usuarios.py
@@ -2,13 +2,14 @@ from libpythonpro.spam.models import Usuario
 
 
 def test_salvar_usuario(sessao):
-    usuario = Usuario (nome='joao')
+    usuario = Usuario(nome='joao', email='joaogarciadelimaneto@gmail.com')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
 
 
 def test_listar_usuario(sessao):
-    usuarios = [Usuario(nome='joao'), Usuario(nome='jones')]
+    usuarios = [Usuario(nome='joao', email='joaogarciadelimaneto@gmail.com'),
+                Usuario(nome='jones', email='joaogarciadelimaneto@gmail.com')]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
